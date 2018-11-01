f1b332d30fe79050a1673699232f075c9e38f981
shymaSane <shymaSane1@gmail.com>
2018-11-01 14:28:37 +0200

    solution achived by adding local storage

--- app.js
+++ app.js
@@ -1,35 +1,133 @@
+//Book constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn
+}
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    //we need to init here so it wont refresh everytime we click submit and delete info
-    const storage = new Stack;
-    //init publish class
-    const publish = new Publish;
-    let currentStorage = storage.get()
-    console.log(currentStorage)
-    //when click on submit button bring data
-    submit_book = document.getElementById('submit_book')
-    submit_book.addEventListener('click', (e) =>{
-        e.preventDefault();
-        //init book > we init here so we can get data when click otherwise it ll give us empty data
-        const book_info = new Book();
-        let book_object = book_info.getbook();
-        
-        if(book_object !== "Not complete"){
-            //current element in the storage without refresh
-            storage.push(book_object)
-            let currentStorage = storage.get()
-         
-            //show current element in the storage
-            publish.showBook(currentStorage)
-            //reset form 
-            document.getElementById("form").reset();
+//Ui constructor
+function UI(){
+
+}
+
+UI.prototype.add = function(book){
+    //get tbody
+    const list = document.getElementById('book_list');
+    //creat new table row
+    let row = document.createElement('tr');
+    //append html to the new row
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `
+    list.appendChild(row)
+    
+}
+
+//clear fields
+UI.prototype.clear = function () {
+    document.getElementById('book_title').value = "";
+    document.getElementById('author_name').value = "";
+    document.getElementById('isbn_number').value = ""
+}
+
+//delete
+UI.prototype.delete = function(target){
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+} 
+
+
+//local storage class
+function Store(){}
+
+Store.prototype.getBooks = function(){
+    //init books variable
+    let books;
+    //if the store is empty then assign the books to an array
+    if(localStorage.getItem('books') === null){
+         books = []
+    } 
+    // if there is a stored books then bring thme but  you to need to pares the results
+    else {
+         books = JSON.parse(localStorage.getItem('books'))
+    }
+    //return the storage
+    return books;
+}
+
+Store.prototype.addBooks = function(book){
+    //get books from the storage
+    const books = this.getBooks();
+    //add the new book
+    books.push(book);
+    //save the books array in the storage again: note you have to stringify the books array
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+Store.prototype.displayBooks = function(){
+    //get books from the storage
+    const books = this.getBooks();
+    //init UI class
+    const ui = new UI();
+    //display books array using the UI class methods 
+    books.forEach(book => {
+        ui.add(book)
+    });
+}
+
+Store.prototype.deleteBook = function(isbn){
+   const books = this.getBooks()
+   books.map((book,i) => {
+        if(book.isbn === isbn){
+            books.splice(i,1)
         }
-    })
+   })
 
-    //Delete button works when we have saved information
-    // let close = document.getElementById('close');
-    // close.addEventListener('click', () => {
-    //     storage.pop()
-    // })
+   localStorage.setItem('books', JSON.stringify(books));
+}
 
- });
+//when document loaded show whatever in the local Store
+document.addEventListener('DOMContentLoaded', () => {
+    const store = new Store;
+    store.displayBooks() 
+})
+
+
+//Event Listeners:
+document.getElementById('form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    //bring input values
+    const title = document.getElementById('book_title').value,
+        author = document.getElementById('author_name').value,
+        isbn = document.getElementById('isbn_number').value
+
+    //init book class
+    let book = new Book(title, author, isbn);
+    //init UI class 
+    let ui = new UI;
+    ui.add(book)
+    //store int in the sotrage
+    let store = new Store()
+    store.addBooks(book)
+    //clear fields
+    ui.clear()
+})
+
+//Event listner for delelte:
+// we need delegation:
+document.getElementById('book_list').addEventListener('click', (e) =>{
+    //init UI class
+    ui = new UI;
+    //call delete method 
+    ui.delete(e.target)
+    console.log(e.target)
+    //init store
+    const store = new Store();
+    //delete from store
+    store.deleteBook(e.target.parentElement.previousElementSibling.textContent)
+})
+
